31a51ecd6c3ca1f814d39e28da9c551156d3be02
fxst1 <faustin.jacquemain@hotmail.fr>
2016-08-05 16:10:16 +0200

    spliter norme (except 42 headers)

---
+++ new_spliter/src/spliter/get_next_command.c
@@ -0,0 +1,95 @@
+#include <spliter.h>
+
+/*
+**	Creer a partir d'une liste de char* un tableau de char**
+**	Puis ajoute notre nouvel argv dans une autre liste qui est la pipeline
+*/
+
+static void				my_pipeline(t_list **ptr, t_list **cmd)
+{
+	char	**t;
+	t_list	*new;
+	t_list	*add;
+
+	add = *ptr;
+	t = lst_to_tab(*cmd);
+	new = ft_lstnew_noalloc(t, sizeof(t));
+	if ((add = *ptr))
+	{
+		while (add->next)
+			add = add->next;
+		add->next = new;
+	}
+	else
+		*ptr = new;
+	*cmd = NULL;
+}
+
+static void				add_pipe(t_norme_com *n, t_command *com, t_list **tmp)
+{
+	if (*n->cpy)
+		ft_lstadd(tmp, ft_lstnew_noalloc(n->cpy, ft_strlen(n->cpy)));
+	n->cpy = ft_strnew(1024);
+	n->str = (char*)n->cpy;
+	my_pipeline(&com->pipeline, tmp);
+	(*n->cmd)++;
+}
+
+static void				add_argument(t_norme_com *n, t_list **tmp)
+{
+	while (**n->cmd == ' ')
+		(*n->cmd)++;
+	if (*n->cpy)
+		ft_lstadd(tmp, ft_lstnew_noalloc(n->cpy, ft_strlen(n->cpy)));
+	n->cpy = ft_strnew(1024);
+	n->str = (char*)n->cpy;
+}
+
+static void				end_gnc(t_norme_com *n, t_command *com, t_list **tmp)
+{
+	if (*n->cpy)
+		ft_lstadd(tmp, ft_lstnew_noalloc(n->cpy, ft_strlen(n->cpy)));
+	else
+		free(n->cpy);
+	if (tmp)
+		my_pipeline(&com->pipeline, tmp);
+	ft_memset(&com->redirs, 0, sizeof(t_redirections));
+	if (is_a_redirection(*n->cmd))
+	{
+		build_redirection(&com->redirs, n->cmd);
+		com->need_redir = 1;
+	}
+}
+
+/*
+**	Renvoie la prochaine commande
+**	si a la fin de l'appel on as encore des caracteres alphabetiques
+**		il y a une syntax erreur
+**	sinon
+**		OK
+**
+**	cmd est l'adrress d'une chaine de caractere
+**	ex:get_next_command(&cmd)
+*/
+
+t_command				get_next_command(char **cmd)
+{
+	t_norme_com	n;
+	t_list		*tmp;
+	t_command	com;
+
+	init_norme_com(&n, cmd);
+	tmp = NULL;
+	ft_memset(&com, 0, sizeof(t_command));
+	while (**n.cmd && !is_a_redirection(*n.cmd) && **n.cmd != ';')
+	{
+		if (**n.cmd == '|')
+			add_pipe(&n, &com, &tmp);
+		else if (**n.cmd != ' ')
+			*(n.str++) = *((*n.cmd)++);
+		else
+			add_argument(&n, &tmp);
+	}
+	end_gnc(&n, &com, &tmp);
+	return (com);
+}
---
+++ new_spliter/src/spliter/hdoc.c
@@ -0,0 +1,45 @@
+#include "../../include/minishell.h"
+#include <spliter.h>
+
+/*
+**	J'ouvre un fd et le ferme pour sauvegarder son contenu
+*/
+
+static void				hdoc(char *eof, int fd)
+{
+	t_it			*it;
+
+	it = init_it_struct();
+	it->eof = 1;
+	ft_putstr("> ");
+	while (read(0, &it->buffer, 4))
+	{
+		parse_line(it);
+		if (it->buffer == '\n')
+		{
+			if (it->line)
+			{
+				if (!ft_strcmp(it->line, eof))
+					break ;
+				it->i = 0;
+				it->buffer = 0;
+				ft_putendl_fd(it->line, fd);
+				it->len = 0;
+				it->line = 0;
+			}
+			ft_putstr("\n> ");
+		}
+		it->buffer = 0;
+	}
+	it->eof = 0;
+	close(fd);
+}
+
+void					prepare_hdoc(t_redirections *t, t_redirection *r)
+{
+	r->fd = open(HDOC_TMP_FILENAME, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	hdoc(r->filename, r->fd);
+	r->fd = open(HDOC_TMP_FILENAME, O_RDWR | O_RDONLY);
+	write(1, "\n", 1);
+	ft_lstadd(&t->in, ft_lstnew_noalloc(r, sizeof(t_redirection)));
+}
